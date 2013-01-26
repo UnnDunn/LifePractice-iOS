0ba2392a1b07ce762b33762b691e47792b6af637
Uchendu Nwachuku <uchendu@unndunn.com>
2013-01-26 16:23:52 -0500

    Unit test for Habit entity. Case 239.

---
+++ LifePractice/LifePracticeTests/UsingHabitEntity.h
@@ -0,0 +1,13 @@
+//
+//  UsingHabitEntity.h
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 1/25/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UsingHabitEntity : SenTestCase
+
+@end
---
+++ LifePractice/LifePracticeTests/UsingHabitEntity.m
@@ -0,0 +1,27 @@
+//
+//  UsingHabitEntity.m
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 1/25/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import "UsingHabitEntity.h"
+
+@implementation UsingHabitEntity
+
+-(void)testNewHabitWithoutDescriptionIsProperlyInitialized
+{
+    LPHabit *habit = [[LPHabit alloc] init];
+    STAssertTrue([[habit Name] isEqualToString: NSLocalizedString(@"Habit_Name_Default", "The default name for a new habit, e.g. 'Do Something'")], @"Habit name should be the default.");
+    STAssertNil([habit Description], @"Habit Description Is Not Nil.");
+    NSTimeInterval *dateCreatedInterval = [[habit CreatedDate] timeIntervalSinceNow];
+    STAssertTrue(abs((int)dateCreatedInterval) < 1, @"Habit Created date not should be set to within 1 second of current time.");
+    int currentHour = [[NSDate date] hour];
+    STAssertTrue([[habit timeOfDay] startHour] <= currentHour, @"Habit time of day should start before current time of day");
+    STAssertTrue([[habit timeOfDay] endHour] > currentHour, @"Habit time of day should end after current time of day");
+    STAssertNil([habit skippedDays], @"Habit Skipped Days should be nil.");
+    STAssertTrue([[habit performances] count] == 0, @"Habit performances list should be nil");
+}
+
+@end
