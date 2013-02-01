6afe27307462a6665e6929d1a14d4c84b3de862e
Uchendu Nwachuku <developer@unndunn.com>
2013-01-31 20:25:41 -0500

    Added DateUtilities test file.

---
+++ LifePractice/LifePracticeTests/DateUtilities.h
@@ -0,0 +1,13 @@
+//
+//  DateUtilities.h
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 1/31/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DateUtilities : SenTestCase
+
+@end
---
+++ LifePractice/LifePracticeTests/DateUtilities.m
@@ -0,0 +1,13 @@
+//
+//  DateUtilities.m
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 1/31/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import "DateUtilities.h"
+
+@implementation DateUtilities
+
+@end
