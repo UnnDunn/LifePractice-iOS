fcc6ca34de57134d5dcba3e4c1df652b21f1d01a
Uchendu Nwachuku <developer@unndunn.com>
2013-02-03 12:28:32 -0500

    Added test suit for LPPerformance. Case 242.

---
+++ LifePractice/LifePracticeTests/UsingLPPerformanceType.h
@@ -0,0 +1,14 @@
+//
+//  UsingLPPerformanceType.h
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 2/3/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "LPPerformance.h"
+
+@interface UsingLPPerformanceType : SenTestCase
+
+@end
---
+++ LifePractice/LifePracticeTests/UsingLPPerformanceType.m
@@ -0,0 +1,22 @@
+//
+//  UsingLPPerformanceType.m
+//  LifePractice
+//
+//  Created by Uchendu Nwachuku on 2/3/13.
+//  Copyright (c) 2013 Uchendu Nwachuku. All rights reserved.
+//
+
+#import "UsingLPPerformanceType.h"
+
+@implementation UsingLPPerformanceType
+
+-(void)testPerformanceInitializesFromNSXMLElementCorrectly
+{
+    
+}
+
+-(void)testPerformanceExportsToNSXMLElementCorrectly
+{
+    
+}
+@end
