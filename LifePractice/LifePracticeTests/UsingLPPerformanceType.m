e25c2e325c67624a52431e349ef08262a05d57d4
Uchendu Nwachuku <developer@unndunn.com>
2013-02-06 23:02:52 -0500

    Added sample files for Habit and Performance.

--- LifePractice/LifePracticeTests/UsingLPPerformanceType.m
+++ LifePractice/LifePracticeTests/UsingLPPerformanceType.m
@@ -7,15 +7,34 @@
 //
 
 #import "UsingLPPerformanceType.h"
+#import <Security/Security.h>
 
 @implementation UsingLPPerformanceType
 
 -(void)testPerformanceInitializesFromNSXMLElementCorrectly
 {
-    
+    NSTimeInterval nowInterval = [[NSDate date] timeIntervalSince1970];
+    NSMutableDictionary *inputDates = [[NSMutableDictionary alloc] initWithCapacity:5];
+    // create 5 random reference/create date pairs
+    for (int i = 0; i < 4; i++) {
+        NSTimeInterval randCreateInterval = arc4random_uniform(nowInterval);
+        NSTimeInterval randRefInterval = randCreateInterval - arc4random_uniform(randCreateInterval);
+        
+        NSDate *randCreateDate = [NSDate dateWithTimeIntervalSince1970:randCreateInterval];
+        NSDate *randRefDate = [DateUtilities getMidnightOfDate:[NSDate dateWithTimeIntervalSince1970:randRefInterval]];
+        
+        [inputDates setObject:randCreateDate forKey:randRefDate];
+        
+        //create an NSXMLElement "Performance" with attribute "createDate" and subelement "ReferenceDate"        
+    }
 }
 
 -(void)testPerformanceExportsToNSXMLElementCorrectly
+{
+    
+}
+
+-(NSUInteger)random
 {
     
 }
