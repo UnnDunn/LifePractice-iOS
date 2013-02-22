729b5df4240e6bbe8568922ae17880f840cdf984
Uchendu Nwachuku <developer@unndunn.com>
2013-02-22 03:33:58 -0500

    Worked on method to init Habit with XML.

--- LifePractice/LifePracticeCore/Entities/LPHabit.m
+++ LifePractice/LifePracticeCore/Entities/LPHabit.m
@@ -7,12 +7,12 @@
 //
 
 #import "LPHabit.h"
+#import "DDXML.h"
 
 @implementation LPHabit
 @synthesize habitName, habitDescription, createdDate, skippedDays, timeOfDay;
 
 NSMutableDictionary *performances;
-
 - init
 {
     if(self = [super init])
@@ -37,8 +37,70 @@ NSMutableDictionary *performances;
 
 -(id)initWithXML:(NSString *)xml
 {
+    DDXMLDocument *xmlDocument = [[DDXMLDocument alloc] initWithXMLString:xml options:0 error:nil];
+    DDXMLElement *root = [xmlDocument rootElement];
     if(self = [super init])
     {
+        [self setHabitName:[[root nodesForXPath:@"Title" error:nil][0] stringValue]];
+        [self setHabitDescription:[[root nodesForXPath:@"Description" error:nil][0] stringValue]];
+        
+        DDXMLElement *timeOfDayElement = [root nodesForXPath:@"TimeFrame" error:nil][0];
+        NSString *startHour = [[timeOfDayElement attributeForName:@"StartHour"] stringValue];
+        NSString *endHour = [[timeOfDayElement attributeForName:@"EndHour"] stringValue];
+        timeOfDay.startHour = [startHour integerValue];
+        timeOfDay.endHour = [endHour integerValue];
+        
+        DDXMLElement *skipDaysContainer = [root nodesForXPath:@"SkipDays" error:nil][0];
+        NSArray *skipDaysElements = [skipDaysContainer children];
+        
+        NSIndexSet *skippedDayElements = [skipDaysElements indexesOfObjectsPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+            DDXMLElement *skippedDay = (DDXMLElement *)obj;
+            NSString *value = [[skippedDay stringValue] lowercaseString];
+            return [value isEqualToString:@"true"];
+        }];
+        
+        NSArray *daysToSkip = [skipDaysElements objectsAtIndexes:skippedDayElements];
+        
+        NSUInteger skippedDaysValue = 0;
+        
+        for (DDXMLElement *element in daysToSkip) {
+            NSString *elementName = [[element name] lowercaseString];
+            if ([elementName isEqualToString:@"sunday"]) {
+                skippedDaysValue |= LPWeekdaySunday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"monday"]) {
+                skippedDaysValue |= LPWeekdayMonday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"tuesday"]) {
+                skippedDaysValue |= LPWeekdayTuesday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"wednesday"]) {
+                skippedDaysValue |= LPWeekdayWednesday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"thursday"]) {
+                skippedDaysValue |= LPWeekdayThursday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"friday"]) {
+                skippedDaysValue |= LPWeekdayFriday;
+                continue;
+            }
+            if ([elementName isEqualToString:@"saturday"]) {
+                skippedDaysValue |= LPWeekdaySaturday;
+                continue;
+            }
+        }
+        skippedDays = skippedDaysValue;
+        
+        NSArray *performanceElements = [root nodesForXPath:@"Performances/Performance" error:nil];
+        for (DDXMLElement *performanceElement in performanceElements) {
+            LPPerformance *newPerformance = [[LPPerformance alloc] initWithXML:[performanceElement XMLString]];
+            [performances setObject:newPerformance forKey:[newPerformance referenceDate]];
+        }
     }
     
     return self;    
